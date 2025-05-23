6b8aabf94179bdda3ecec85df452fb8472499e86
EliDaher <eeli56315@gmail.com>
2025-05-23 16:56:03 +0300

    origin *

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 
 // السماح بالوصول من الشبكة المحلية
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
